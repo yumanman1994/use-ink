8ff6e37025a13374f09d0586959d719bc432af2d
余志成 <1029686739@qq.com>
2020-12-17 22:19:35 +0800

    feat: ✨  新增create-cmp组件

--- source/components/App.tsx
+++ source/components/App.tsx
@@ -1,5 +1,16 @@
+import { render } from 'ink';
 import meow from 'meow';
+import React from 'react';
+import CreateCmp from './commands/create-cmp';
 
+export const enum COMMANDS {
+  INIT_CONFIG = 'init',
+  START_DEV = 'start',
+  CREATE_PROJECT = 'create',
+  CREATE_CMP = 'create-cmp',
+  UPDATE_DEPS = 'update-deps',
+  MERGE_BACK = 'merge-back',
+}
 export default function App(
   cli: meow.Result<{
     version: {
@@ -13,6 +24,8 @@ export default function App(
   }>
 ) {
   const { input, flags } = cli;
+  // 取第一条当命令
+  const command = input[0];
   if (input.length === 0) {
     if (flags.version) {
       cli.showVersion();
@@ -22,7 +35,15 @@ export default function App(
       cli.showHelp(0);
     }
     // 没有任何参数和标志，输出帮助信息
-    cli.showHelp(1);
+    return cli.showHelp(1);
+  } else {
+    switch (command) {
+      case COMMANDS.CREATE_CMP:
+        return render(<CreateCmp />);
+
+      default:
+        cli.showHelp(0);
+        return null;
+    }
   }
-  console.log(input);
 }
---
+++ source/components/commands/create-cmp/create-cmp.tsx
@@ -0,0 +1,10 @@
+import { Box, Text } from 'ink';
+import React from 'react';
+
+export default function CreateCmp() {
+  return (
+    <Box flexDirection="column">
+      <Text color="green">[TODO] Command: Create cmp</Text>
+    </Box>
+  );
+}
